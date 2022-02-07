4602415b272994537a0701fc7d75568696e49a49
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2022-02-07 10:21:17 +0300

    area login.

--- MyAutoFix/Areas/Admin/Controllers/ManageController.cs
+++ MyAutoFix/Areas/Admin/Controllers/ManageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyAutoFix.Areas.Admin.Controllers
 {
@@ -9,6 +10,12 @@ namespace MyAutoFix.Areas.Admin.Controllers
             return View();
         }
         public IActionResult User()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login()
         {
             return View();
         }
