7c6dfcb2b893957576d79aaf3078a4b1a0a25d71
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2022-02-08 09:14:29 +0300

    Login Logout islemleri

--- MyAutoFix/ViewModels/LoginViewModel.cs
+++ MyAutoFix/ViewModels/LoginViewModel.cs
@@ -4,8 +4,11 @@ namespace MyAutoFix.ViewModels
 {
     public class LoginViewModel
     {
+
+        [Display(Name = "Kullanıcı Adı")]
         [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir.")]
         public string UserName { get; set; }
+
         [Required(ErrorMessage = "Şifre alanı gereklidir.")]
         //[StringLength(100),MinLength(6,ErrorMessage ="Şifreniz minimum 6 karekterli olmalıdır.")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifreniz minimum 6 karekterli olmalıdır.")]
@@ -13,5 +16,7 @@ namespace MyAutoFix.ViewModels
         [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Display(Name = "Beni Hatırla")]
+        public bool RememberMe { get; set; }
     }
 }
